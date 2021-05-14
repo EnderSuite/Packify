6b9646b6c6f8ecdb5cfd8b85d73c01c1275b632a
maximilianheidenreich <github@maximilian-heidenreich.de>
2021-05-14 18:33:10 +0200

    Updated NetworkManager to new collectable handling

--- src/main/java/com/endersuite/packify/NetworkManager.java
+++ src/main/java/com/endersuite/packify/NetworkManager.java
@@ -1,14 +1,18 @@
 package com.endersuite.packify;
 
+import com.endersuite.libcore.strfmt.Level;
+import com.endersuite.libcore.strfmt.StrFmt;
 import com.endersuite.packify.events.PacketReceivedEvent;
-import com.endersuite.packify.transmission.CollectableTransmission;
+import com.endersuite.packify.handlers.CollectablePacketHandler;
+import com.endersuite.packify.transmission.Transmission;
 import de.maximilianheidenreich.jeventloop.EventLoop;
 import lombok.Getter;
 import org.jgroups.JChannel;
 import org.jgroups.Message;
 
 import java.util.UUID;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
 
 /**
  * TODO: Add docs
@@ -21,29 +25,31 @@ public class NetworkManager extends APacketDelegator {
     // ======================   VARS
 
     /**
-     * The NetworkManager singleton.
-     */
-    private static NetworkManager instance;
-
-    /**
      * The configured {@link JChannel} instance.
      */
     @Getter
     private final JChannel jChannel;
 
-    //private Map<UUID, >
+    @Getter
+    private final CollectablePacketHandler collectablePacketHandler;
+
+    @Getter
+    private final ScheduledExecutorService scheduler;
 
 
     // ======================   CONSTRUCTOR
 
-    private NetworkManager(EventLoop eventLoop, String nodeName) throws Exception {
+    public NetworkManager(EventLoop eventLoop, String nodeName) throws Exception {
         super(eventLoop);
 
         eventLoop.removeEventHandler(PacketReceivedEvent.class, this::handlePacketReceivedEvent);   // Make sure packets are not handled twice or more times
         eventLoop.addEventHandler(PacketReceivedEvent.class, this::handlePacketReceivedEvent);
 
+        Transmission.setDefaultNetworkManager(this);
         this.jChannel = new JChannel();
-        this.jChannel.setReceiver(new DefaultReceiver(eventLoop));
+        this.scheduler = Executors.newSingleThreadScheduledExecutor();
+        this.jChannel.setReceiver(new DefaultReceiver(this));
+        this.collectablePacketHandler = new CollectablePacketHandler(this);
         //this.jChannel.setDiscardOwnMessages(true);
 
         if ("default".equalsIgnoreCase(nodeName))
@@ -77,68 +83,15 @@ public class NetworkManager extends APacketDelegator {
     // ======================   HELPERS
 
     /**
-     * Sends a message over the active JChannel..
+     * Sends a message over the active JChannel.
      *
      * @param message
      *          The message to send
      * @throws Exception
      */
     public void sendRaw(Message message) throws Exception {
+        new StrFmt("{prefix} Sending: " + message.getObject()).setLevel(Level.TRACE).toLog();
         getJChannel().send(message);
-    }
-
-
-/*
-    public void broadcastRaw(APacket packet, boolean loopback) throws Exception {
-        Message message = new ObjectMessage(null, packet);
-        if (!loopback)
-            message.setFlag(Message.TransientFlag.DONT_LOOPBACK);
-        sendRaw(message);
-    }
-
-    public <T extends ACollectableResponsePacket> CompletableFuture<List<? extends ACollectableResponsePacket>> broadcastCollect(ACollectableRequestPacket<T> packet, boolean loopback) throws Exception {
-        CompletableFuture<List<? extends ACollectableResponsePacket>> callback = new CompletableFuture<>();
-        CollectablePacketHandler.getCollectionCallbacks().put(packet.getCollectId(), callback);
-        broadcastRaw(packet, loopback);
-        return callback;
-    }*/
-
-
-    // ======================   GETTER & SETTER
-
-    /**
-     * Returns the NetworkManager singleton instance.
-     * Note: Also creates one if none exists.
-     *
-     * @return The NetworkManager instance.
-     */
-    public static NetworkManager getInstance() {
-        return NetworkManager.instance;
-
-        /*try {
-            NetworkManager.instance = new NetworkManager();
-
-            // Reset stuff -> We do this because something like a server reload or somebody
-            // could fuck it up and we would have dangling callbacks, packets etc.
-            // Also, we need to initialize these utility classes!
-            //CollectableTransmission.pendingTransmissions = new ConcurrentHashMap<>();
-
-            return NetworkManager.instance;
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            new StrFmt("{prefix} Could not initialize JGroup networking! Database fallback will be used! This might affect performance!").setLevel(Level.ERROR).toConsole();
-            return null;
-        }*/
-
-    }
-
-    public static NetworkManager initialize(EventLoop eventloop, String nodeName) throws Exception {
-        // Reset stuff -> We do this because something like a server reload or somebody
-        // could fuck it up and we would have dangling callbacks, packets etc.
-        // Also, we need to initialize these utility classes!
-        CollectableTransmission.pendingTransmissions = new ConcurrentHashMap<>();
-
-        return new NetworkManager(eventloop, nodeName);
     }
 
 }
